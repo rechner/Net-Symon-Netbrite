540e089001453e6ed9f5d7c9c0aea8bc51734ccb
Jay Smith <jayvsmith@gmail.com>
2015-05-09 12:12:07 -0500

    Initial XMPP client code

--- signserver.pl
+++ signserver.pl
@@ -6,84 +6,93 @@ use lib 'lib';
 use Net::Symon::NetBrite qw(:constants);
 use Net::Symon::NetBrite::Zone;
 
-my $sign = Net::Symon::NetBrite->new(
-    address => '10.150.3.149',
-);
+use Net::XMPP;
+use Config::Simple;
 
-my $hostwidth = 50;
+my $cfg = new Config::Simple('app.ini');
 
-$sign->zones(
-    alertnum => Net::Symon::NetBrite::Zone->new(
-        rect => [0, 1, 20, 17],
-        default_font => 'monospace_16',
-        default_color => COLOR_YELLOW,
-        initial_text => '{scrolloff}{center}?',
-    ),
-    alertlabel => Net::Symon::NetBrite::Zone->new(
-        rect => [0, 19, 20, 23],
-        default_font => 'proportional_5',
-        default_color => COLOR_YELLOW,
-        initial_text => '{scrolloff}{right}ALRM',
-    ),
-    line1host => Net::Symon::NetBrite::Zone->new(
-        rect => [21, 0, $hostwidth, 11],
-        default_font => 'proportional_11',
-        default_color => COLOR_GREEN,
-        scroll_speed => SCROLL_SLOW,
-    ),
-    line1 => Net::Symon::NetBrite::Zone->new(
-        rect => [$hostwidth + 1, 0, 199, 11],
-        default_font => 'proportional_11',
-        default_color => COLOR_GREEN,
-        scroll_speed => SCROLL_SLOW,
-    ),
-    line2host => Net::Symon::NetBrite::Zone->new(
-        rect => [21, 12, $hostwidth, 17],
-        default_font => 'proportional_5',
-        default_color => COLOR_GREEN,
-        scroll_speed => SCROLL_SLOW,
-    ),
-    line2 => Net::Symon::NetBrite::Zone->new(
-        rect => [$hostwidth + 1, 12, 199, 17],
-        default_font => 'proportional_5',
-        default_color => COLOR_GREEN,
-        scroll_speed => SCROLL_SLOW,
-    ),
-    line3host => Net::Symon::NetBrite::Zone->new(
-        rect => [21, 18, $hostwidth, 23],
-        default_font => 'proportional_5',
-        default_color => COLOR_GREEN,
-        scroll_speed => SCROLL_SLOW,
-    ),
-    line3 => Net::Symon::NetBrite::Zone->new(
-        rect => [$hostwidth + 1, 18, 199, 23],
-        default_font => 'proportional_5',
-        default_color => COLOR_GREEN,
-        scroll_speed => SCROLL_SLOW,
-    ),
-);
+# my $sign = Net::Symon::NetBrite->new(
+#     address => $cfg->param('NETBRITE.address')
+# );
 
-<>;
-$sign->message('alertnum', '{scrolloff}{center}{green}0');
-$sign->message('alertlabel', '{scrolloff}{yellow}ALRM');
-$sign->message('line1host', '.');
-$sign->message('line1', '.');
-$sign->message('line2host', '.');
-$sign->message('line2', '.');
-$sign->message('line3host', '.');
-$sign->message('line3', '.');
-<>;
-$sign->message('alertnum', '{scrolloff}{center}{red}1');
-$sign->message('alertlabel', '{scrolloff}{red}{blinkon}ALRM', { sound_alarm => 1 });
-<>;
-$sign->message('line1host', '{scrolloff}{red}pr0.nycx');
-$sign->message('line1', '{red}An LP CAM partition is full.');
-<>;
-$sign->message('line2host', '{scrolloff}{yellow}co1');
-$sign->message('line2', '{yellow}Logical link down on e1/8 (412 FSO 2)');
-<>;
-$sign->message('line3host', '{scrolloff}{yellow}sofy.gvpn');
-$sign->message('line3', '{yellow}High CPU for multiple processes.');
-<>;
-$sign->reboot();
+# $sign->zones(
+#    main  => Net::Symon::NetBrite::Zone->new(
+#        rect => [0, 1, 160, 16],
+#        default_font => 'proportional_5',
+#        default_color => COLOR_RED,
+#    ));
+
+# <>;
+# $sign->message('line1', 'Monkeys are awesome');
+# <>;
+# $sign->message('line5', '{red}The {green}Quick {red}Brown Fox Jumps Over the Lazy Dog');
+# $sign->message('line1', 'Monkeys are awesome');
+# <>;
+
+my $server = $cfg->param('XMPP.server');
+my $port = $cfg->param('XMPP.port');
+my $username = $cfg->param('XMPP.username');
+my $password = $cfg->param('XMPP.password');
+my $resource = $cfg->param('XMPP.resource');
+
+my $Connection = new Net::XMPP::Client(debuglevel=>0, debugfile=>'stdout');
+$Connection->SetCallBacks(message=>\&InMessage);
+my $status = $Connection->Connect(hostname=>$server,
+                                  componentname=>$server,
+                                  port=>$port,
+                                  srv=>1,
+                                  tls=>1,
+                                  ssl_ca_path=>'/etc/ssl/certs');
+
+$SIG{HUP} = \&Stop;
+$SIG{KILL} = \&Stop;
+$SIG{TERM} = \&Stop;
+$SIG{INT} = \&Stop;
+
+if (!(defined($status)))
+{
+    print "ERROR:  Jabber server is down or connection was not allowed.\n";
+    print "        ($!)\n";
+    exit(0);
+}
+
+my @result = $Connection->AuthSend(username=>$username,
+                                   password=>$password,
+                                   resource=>$resource);
+
+if ($result[0] ne "ok")
+{
+    print "ERROR: Authorization failed: $result[0] - $result[1]\n";
+    exit(0);
+}
+
+$Connection->RosterGet();
+$Connection->PresenceSend();
+while(defined($Connection->Process())) { }
+
+exit(0);
+
+sub Stop
+{
+    print "Exiting...\n";
+    $Connection->Disconnect();
+    exit(0);
+}
+
+sub InMessage
+{
+    my $sid = shift;
+    my $message = shift;
+    
+    my $type = $message->GetType();
+    my $fromJID = $message->GetFrom("jid");
+    
+    my $from = $fromJID->GetUserID();
+    my $resource = $fromJID->GetResource();
+    my $body = $message->GetBody();
+    print "===\n";
+    print "Message ($type)\n";
+    print "  From: $from ($resource)\n";
+    print "  Body: $body\n";
+}
 
