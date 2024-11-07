4aebc18606b372b4cb85859507db8e74f4e2cccc
Rechner Fox <659028+rechner@users.noreply.github.com>
2024-11-06 18:32:02 -0800

    Add MQTT dooble

--- foxsign.pl
+++ foxsign.pl
@@ -6,33 +6,57 @@ use lib 'lib';
 use Net::Symon::NetBrite qw(:constants);
 use Net::Symon::NetBrite::Zone;
 
+use Net::MQTT::Simple;
+
 use Config::Simple;
 
+sub main {
+    my $cfg = new Config::Simple('app.ini');
 
-my $cfg = new Config::Simple('app.ini');
 
-my $sign = Net::Symon::NetBrite->new(
-    address => $cfg->param('NETBRITE.address')
-);
+    my $mqtt = Net::MQTT::Simple->new($cfg->param('MQTT.address'));
 
-$sign->zones(
-    main  => Net::Symon::NetBrite::Zone->new(
-       rect => [0, 1, 200, 16],
-        default_font => 'monospace_16',
-        default_color => COLOR_YELLOW,
-    ),
-    second => Net::Symon::NetBrite::Zone->new(
-        rect => [0, 17, 200, 32],
-        default_font => 'proportional_9',
-        default_color => COLOR_YELLOW,
-        
-    ),
-);
+    #$mqtt->retain( "topic/here" => "Message here");
+    
+    my $sign = Net::Symon::NetBrite->new(
+        address => $cfg->param('NETBRITE.address')
+    );
 
-my $message = "{red}{scrolloff}Upcoming Events";
-$sign->message('main', $message);
+    $sign->zones(
+        main  => Net::Symon::NetBrite::Zone->new(
+           rect => [0, 1, 200, 16],
+            default_font => 'monospace_16',
+            default_color => COLOR_YELLOW,
+        ),
+        second => Net::Symon::NetBrite::Zone->new(
+            rect => [0, 20, 200, 32],
+            default_font => 'proportional_9',
+            default_color => COLOR_YELLOW,
+            
+        ),
+    );
 
-$sign->message('second', "Upcoming events....  {red}3D Modeling 101: {green}Saturday 26 October @ 11:00... {red}Git for Makers: {green}Tuesday 29 October @ 19:00...");
+    $mqtt->run(
+        "/symon/main" => sub {
+            my ($topic, $message) = @_;
+            print "[$topic] $message\n";
+            $sign->message('main', $message);
+        },
+        "/symon/second" => sub {
+            my ($topic, $message) = @_;
+            print "[$topic] $message\n";
+            $sign->message('second', $message);
+        },
+        "/symon/reboot" => sub {
+            my ($topic, $message) = @_;
+            print "[$topic] $message\n";
+            $sign->reboot();
+        }
+    );
 
-exit(0);
 
+
+    exit(0);
+}
+
+main;
